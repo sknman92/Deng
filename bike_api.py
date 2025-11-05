f72fe3f791ad1cd9cb3ae52c3bf77bc895fad628
Charles Yi <133015448+sknman92@users.noreply.github.com>
2025-11-05 15:59:14 +0000

    changes

--- bike_api.py
+++ bike_api.py
@@ -2,7 +2,8 @@ import pandas as pd
 import requests
 import time
 import json
-from datetime import date
+from datetime import datetime
+import logging
 
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
@@ -11,26 +12,39 @@ headers = {
 url = 'https://api.tfl.gov.uk/BikePoint/'
 timeout = 10
 num_tries = 50
-wait_time = 30
+wait_time = 5
+retry_status_codes = [408, 500]
+
+def _save_file(data: json, today: datetime):
+    file_path = f'data/bike_data_{today.strftime("%Y_%m_%d_T%H-%M-%S")}.json'
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+    return file_path
 
 def _api_call():
     response = requests.get(url, headers=headers, timeout=timeout)
-    
+
     if response.status_code == 200:
         print('Successfully connected')
-        today = date.today()
-        
-        return response.json()
-    else:
+        today = datetime.today()
+        today_json = {'today' : str(today)}
+        response_json = response.json()
+        for bikepoint in response_json:
+            bikepoint.update(today_json)
+        return response_json, today
+    elif response.status_code in retry_status_codes:
         raise Exception(f'API error: status code {response.status_code}')
+    else:
+        print('Failed to connect')
 
 def api_call_with_retries():
 
     for attempt in range(1, num_tries + 1):
         try:
-            data = _api_call()
+            data, today = _api_call()
+            file_path = _save_file(data, today)
+            print(f'Successfully saved data into {file_path}')
             return data
-            break
         except Exception as e:
             print(f'Attempt {attempt} failed: {e}')
             if attempt < num_tries:
@@ -38,4 +52,12 @@ def api_call_with_retries():
                 time.sleep(wait_time)
             else:
                 raise Exception(f'Failed to connect after {num_tries} attempts') from e
+
+
+data = api_call_with_retries()
+
+
+
+
+
 
