c7f3cd577bcd418f073350676b73ee7c47b4a20e
Charles Yi <133015448+sknman92@users.noreply.github.com>
2025-11-11 15:01:23 +0000

    combined bike point api

--- bike_point_load.py
+++ bike_point_load.py
@@ -15,33 +15,36 @@ s3_client = boto3.client(
     , aws_secret_access_key=aws_secret_key
 )
 
-try:
-    # fetching list of buckets
-    # nest w. try and except to test s3_client.list_objects_v2 function
-    s3_client.list_objects_v2(Bucket=bucket)
-    
-    files = os.listdir('data/')
-    
-    # collating all files in directory
-    file_list = [os.path.join('data/', file) for file in files if file.endswith('.json')]
+def bike_load():
+    try:
+        # fetching list of buckets
+        # nest w. try and except to test s3_client.list_objects_v2 function
+        s3_client.list_objects_v2(Bucket=bucket)
+        
+        files = os.listdir('data/')
+        
+        # collating all files in directory
+        file_list = [os.path.join('data/', file) for file in files if file.endswith('.json')]
 
-    num_files = len(file_list)
-    # check that there is file        
-    if num_files > 0:
+        num_files = len(file_list)
+        # check that there is file        
+        if num_files > 0:
 
-        # uploading and removing file
-        for file in file_list:
-            s3_filename = os.path.join('python-import', file)
-            s3_client.upload_file(file, bucket, s3_filename)
-            os.remove(file_list[0])
+            # uploading and removing file
+            for file in file_list:
+                s3_filename = os.path.join('python-import', file)
+                s3_client.upload_file(file, bucket, s3_filename)
+                os.remove(file)
 
-        print(f'Successfully loaded {num_files} file(s) to S3 bucket')
-    
-    else: 
-        print('No files found')
+            print(f'Successfully loaded {num_files} file(s) to S3 bucket')
+        
+        else: 
+            print('No files found')
 
-except Exception as e:
-    print(f'Error occured: {e}')
-    raise e
+    except Exception as e:
+        print(f'Error occured: {e}')
+        raise e
 
+if __name__ == '__main__':
+    bike_load()
 
